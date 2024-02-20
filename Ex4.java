99c9f1a1cd0a0baf1d5e38b00d16ed627c681f28
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-02-20 08:34:38 -0300

    Exercicios de Revisão

---
+++ Ex4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ex4 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int anoAtual, anoNasc, idade;
+		String resposta = "sim";
+		
+		while(! resposta.equalsIgnoreCase("NÃO")) {
+			System.out.print("Digite o ano atual: ");
+			anoAtual = in.nextInt();
+			System.out.print("Digite o ano de nascimento: ");
+			anoNasc = in.nextInt();
+			idade = anoAtual-anoNasc;
+			
+			System.out.println("A pessoa tem "+idade+" anos");
+			
+			if(anoAtual-anoNasc>=18) {
+				System.out.println("Maior de idade");
+			} else {
+				System.out.println("Menor de idade");
+			}
+			
+			System.out.println("Deseja calcular a idade de mais uma pessoa? ");
+			resposta = in.next();
+		}
+		
+		
+	}
+}
