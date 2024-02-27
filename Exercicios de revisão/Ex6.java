663e64f2e13239bbc3b3665829fba5064cdb7f21
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-02-27 07:54:11 -0300

    Update Ex6.java

--- Exercicios de revisão/Ex6.java
+++ Exercicios de revisão/Ex6.java
@@ -5,7 +5,11 @@ public class Ex6 {
 		final int TAM = 6;
 		int a[], i, j = 0;
 		a = new int[TAM];
-		
+
+		//solução alternativa é já fazer o
+  //Vetor já definido com os valores
+  //int[] a = {3, 2, 8, 7, 5, 4};
+
 		for(i=0; i<TAM; i++) {
 			System.out.print("Digite o "+(i+1)+"o valor do vetor: ");
 			a[i] = in.nextInt();
