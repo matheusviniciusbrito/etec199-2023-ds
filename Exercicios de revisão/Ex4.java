884c5f16fb0b13aa89e5fa386642c9b926073908
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-02-27 08:46:27 -0300

    Update Ex4.java

--- Exercicios de revisão/Ex4.java
+++ Exercicios de revisão/Ex4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import Java.time.localDate;
+import java.time.localDate;
 public class Ex4 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
