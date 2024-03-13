7afaf39ddc6e6148058340309fc6fcb6b440f295
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-03-13 06:50:52 -0700

    Exercicio 2 finalizadoo

--- Ex de Classes/Exercicio2/src/Funcionario.java
+++ Ex de Classes/Exercicio2/src/Funcionario.java
@@ -1,4 +1,23 @@
 
 public class Funcionario {
-
+	
+	String nome;
+	double salarioBruto;
+	double imposto;
+	double salarioLiquido;
+	
+	
+	double calcSalarioLiquido(double salarioBruto) {
+		salarioLiquido = salarioBruto - (salarioBruto*0.07);
+		
+		return salarioLiquido;
+	}
+	
+	double aumentaSalario(double aumenta) {
+		
+		salarioBruto += aumenta;
+		salarioLiquido = salarioBruto - (salarioBruto*0.07);
+		
+		return salarioLiquido;
+	}
 }
--- Ex de Classes/Exercicio2/src/Main.java
+++ Ex de Classes/Exercicio2/src/Main.java
@@ -1,9 +1,30 @@
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		Funcionario pedro = new Funcionario();
+		System.out.print("Insira o salario bruto de Pedro: ");
+		pedro.salarioBruto = in.nextDouble();
+		pedro.salarioLiquido = pedro.calcSalarioLiquido(pedro.salarioBruto);
+		System.out.println("O salário liquido de Pedro é: "+pedro.salarioLiquido);
+		
+		System.out.print("Insira o valor à adicionar no salário de Pedro: ");
+		double valor = in.nextDouble();
+		pedro.aumentaSalario(valor);
+		System.out.println("O sálario líquido de Pedro após reajuste ficou em: "+pedro.salarioLiquido);
 
+		Funcionario vagner = new Funcionario();
+		System.out.print("Insira o salario bruto de Vagner: ");
+		vagner.salarioBruto = in.nextDouble();
+		vagner.salarioLiquido = vagner.calcSalarioLiquido(vagner.salarioBruto);
+		System.out.println("O salário liquido de Vagner é: "+vagner.salarioLiquido);
+		
+		System.out.print("Insira o valor à adicionar no salário de Vagner: ");
+		double valor = in.nextDouble();
+		vagner.aumentaSalario(valor);
+		System.out.println("O sálario líquido de Vagner após reajuste ficou em: "+vagner.salarioLiquido);
 	}
 
 }
