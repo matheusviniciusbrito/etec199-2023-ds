b2c7d1ca1647113ba5304d9924969118a23b7cbf
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-04-03 08:38:52 -0300

    Exercicios arrumados

--- ExercicioProduto/Main.java
+++ ExercicioProduto/Main.java
@@ -1,24 +1,34 @@
-import java.util.*;
+
+import java.util.Scanner;
 public class Main {
-	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		Produto teste1 = new Produto();
-		System.out.print("Digite o nome do produto: ");
-		teste1.setNome(in.next());
-		System.out.print("Digite o preço de custo do produto: ");
-		teste1.setPrecoCusto(in.nextDouble());
-		System.out.print("Digite o preco de venda do produto: ");
-		teste1.setPrecoVenda(in.nextDouble());
-		//Fazer o código repetir até o usuario inserir o valor correto
-		while(teste1.getPrecoVenda() < teste1.getPrecoCusto()) {
-			teste1.setPrecoVenda(in.nextDouble());
-		}
-		teste1.calcularMargemLucro();
-		System.out.println("Produto: " + teste1.getNome());
-		System.out.println("A margem de Lucro é de: " + teste1.getMargemLucroPorcentagem() + "%");
+		Produto p1 = new Produto();
+		String resposta = "SIM";
 		
-	}
+		while(! resposta.equalsIgnoreCase("NÃO")){
+			System.out.print("Coloque o nome do produto: ");
+			p1.setNome(in.next());
+			
+			System.out.print("Coloque o preço de custo: ");	
+			p1.setPrecoCusto(in.nextDouble());
+			
+			System.out.print("Coloque o preço de venda: ");
+			p1.setPrecoVenda(in.nextDouble());
+			while(p1.getPrecoVenda() < p1.getPrecoCusto()) {
+				p1.setPrecoVenda(in.nextDouble());
+			}
+			
+			
+			System.out.println("");	
+		        System.out.println("Produto: " + p1.getNome());
+		        System.out.printf("Lucro: R$%.2f\n", p1.calcularMargemLucro());
+		        System.out.printf("Porcentagem de lucro: %.2f%%\n", p1.getMargemLucroPorcentagem());
+			System.out.println("");
+			System.out.print("Deseja cadastrar outro produto? ");
+			resposta = in.next();
+			System.out.println();
+		}
 
+    }
 }
