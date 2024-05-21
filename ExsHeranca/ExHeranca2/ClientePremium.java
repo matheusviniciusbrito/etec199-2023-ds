ff36319328dbfb8c60afaa0a3a8c8c9057d63764
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-05-21 18:53:56 -0300

    Update ClientePremium.java

--- ExsHeranca/ExHeranca2/ClientePremium.java
+++ ExsHeranca/ExHeranca2/ClientePremium.java
@@ -5,7 +5,6 @@ public class ClientePremium extends Cliente {
     public double calculaDesconto(double preco) {
         double valorDesconto = preco - (preco*0.15);
         this.setValor_Promocional(valorDesconto);
-        System.out.println("Cliente premium tem desconto de 15% porcento logo o pre;o final fica: " + valorDesconto);
         return valorDesconto;
 	}
 }
