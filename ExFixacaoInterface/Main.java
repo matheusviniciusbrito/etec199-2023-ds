58b68d457b376933c57e55cd7d21c3db5c1d2dbb
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-10-08 13:43:26 -0300

    Comentei um main com Scanner no final do codigo

--- ExFixacaoInterface/Main.java
+++ ExFixacaoInterface/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     	
@@ -36,5 +36,54 @@ public class Main {
         // Aplicando uma taxa de rendimento de 2% (0.02)
         contaPoupanca.aplicarTaxa(0.02); // 1100 + 2% = 1122
         contaPoupanca.verificarSaldo();
+
+        /*
+        //Com Scanner para testes interativos
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\n---- Teste interativo com Scanner ----");
+
+		//conta corrente
+        System.out.println("Digite o saldo inicial da Conta Corrente:");
+        double saldoCorrente = in.nextDouble();
+        System.out.println("Digite o limite especial da Conta Corrente:");
+        double limiteCorrente = in.nextDouble();
+        ContaCorrente ccScanner = new ContaCorrente(saldoCorrente, limiteCorrente);
+        
+        ccScanner.verificarSaldo();
+
+        System.out.println("Digite um valor para depósito na Conta Corrente:");
+        double depositoCC = in.nextDouble();
+        ccScanner.depositarValor(depositoCC);
+        ccScanner.verificarSaldo();
+
+        System.out.println("Digite um valor para saque na Conta Corrente:");
+        double saqueCC = in.nextDouble();
+        ccScanner.sacarValor(saqueCC);
+        ccScanner.verificarSaldo();
+
+        //conta poupança
+        System.out.println("\nDigite o saldo inicial da Conta Poupança:");
+        double saldoPoupanca = in.nextDouble();
+        ContaPoupanca cpScanner = new ContaPoupanca(saldoPoupanca);
+
+        cpScanner.verificarSaldo();
+
+        System.out.println("Digite um valor para depósito na Conta Poupança:");
+        double depositoCP = in.nextDouble();
+        cpScanner.depositarValor(depositoCP);
+        cpScanner.verificarSaldo();
+
+        System.out.println("Digite um valor para saque na Conta Poupança:");
+        double saqueCP = in.nextDouble();
+        cpScanner.sacarValor(saqueCP);
+        cpScanner.verificarSaldo();
+
+        System.out.println("Digite a taxa de rendimento mensal (ex: 0.02 para 2%):");
+        double taxaCP = in.nextDouble();
+        cpScanner.aplicarTaxa(taxaCP);
+        cpScanner.verificarSaldo();
+        */
+        
     }
 }
